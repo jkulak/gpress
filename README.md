769bbd3026ef4cfb59378f79174ecfd0e73c6130
Jakub Kułak <jakub.kulak@gmail.com>
2016-11-26 21:59:41 -0600

    Support inline source parameter

--- README.md
+++ README.md
@@ -7,3 +7,9 @@ Run the build
 ```
 $ docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.6 go build -v
 ```
+
+Work inside the Docker container
+
+```
+$ docker run --rm -ti -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.6 bash
+```
--- main.go
+++ main.go
@@ -2,11 +2,43 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"flag"
+    "io/ioutil"
 )
 
-func main() {
-	fmt.Println("Not enough attributes!")
+// Generic error handling
+func check(e error) {
+    if e != nil {
+        fmt.Println("Problem reading the file!")
+        panic(e)
+    }
+}
 
-	fmt.Println("The time is", time.Now())
+func main() {
+
+    sourceFile := flag.String("sourcefile", "", "a filename")
+
+    flag.Parse()
+
+    if *sourceFile != "" {
+        dat, err := ioutil.ReadFile(*sourceFile)
+        check(err)
+        fmt.Print(string(dat))
+
+        datCompressed := string(dat) + "compression?"
+        datComp := []byte(datCompressed)
+
+        error := ioutil.WriteFile(string(*sourceFile) + ".gpressed", datComp, 0644)
+        check(error)
+    }
+
+
+
+    //  f.Close()
+
+
+    // Debug part
+    fmt.Println("sourceFile:", *sourceFile)
+    fmt.Println("tail:", flag.Args())
+
 }
